51fe8ea4378e5dc1fad440600b2b55f3cc18d561
lucas teotonio <lucas-santos-teo@hotmail.com>
2021-08-09 23:47:34 -0300

    criado base do projeto front

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sgpl-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'frontend';
+}
