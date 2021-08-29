33f0753fc0fedd9137ff801f097b6bfba945f9aa
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-08-29 14:45:16 -0300

    settings in gitignore

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Setting GOOGLE_OAUTH2_CLIENT_ID
 GOOGLE_OAUTH2_CLIENT_ID = 'your-actual-client-id-value'
 
 # Quick-start development settings - unsuitable for production
