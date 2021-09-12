169a9690882a5a6e811a1bd96382c1f69df338e4
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-09-12 11:32:25 -0300

    alterado nome do model Suppliers para Supplier

--- backend/suppliers/models.py
+++ backend/suppliers/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Suppliers(models.Model):
+class Supplier(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     cpf = models.CharField(max_length=11)
