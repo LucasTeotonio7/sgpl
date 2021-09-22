c9fe4c871c84749db814fef5ce32f821baada733
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-09-21 22:19:48 -0300

    configurado mat-paginator para evitar rolagem infinita

--- frontend/src/app/components/supplier/supplier-list/supplier-list.component.ts
+++ frontend/src/app/components/supplier/supplier-list/supplier-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { SharedService } from 'src/app/services/shared.service';
 import { Supplier } from './supplier.model';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'sgpl-supplier-list',
@@ -15,9 +17,14 @@ export class SupplierListComponent implements OnInit {
 
   constructor(private SharedService: SharedService) { }
 
+  dataSource = new MatTableDataSource;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   ngOnInit(): void {
     this.SharedService.getSupplierList().subscribe(data => {
       this.supplier = data;
+      this.dataSource = new MatTableDataSource<Supplier>(this.supplier);
+      this.dataSource.paginator = this.paginator;
     })
   }
 
