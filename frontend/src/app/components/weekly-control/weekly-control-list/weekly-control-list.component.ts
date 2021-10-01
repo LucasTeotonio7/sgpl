99017e72bf1fe36562ff3112563979bb7b8a5aa9
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-09-30 23:45:57 -0300

    implementado list de controle semanal

--- frontend/src/app/components/weekly-control/weekly-control-list/weekly-control-list.component.ts
+++ frontend/src/app/components/weekly-control/weekly-control-list/weekly-control-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { WeeklyControlService } from '../services/weekly-control.service'
 
 @Component({
   selector: 'sgpl-weekly-control-list',
@@ -8,7 +9,7 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class WeeklyControlListComponent implements OnInit {
 
-  dataSource = new MatTableDataSource;
+
 
   test: any = [
     {name_supplier:'Lorem Ipsum',sat: '23',sun: '12',mon: '14',tue: '17',wed: '29',thu: '20',fri: '21',qty: '136',product_price: '1.95',total_price: '265.2',status: 'OK'},
@@ -16,12 +17,18 @@ export class WeeklyControlListComponent implements OnInit {
 
   ];
 
-  displayedColumns = ['name_supplier','sat','sun','mon','tue','wed','thu','fri','qty','product_price','total_price','status','action'];
+  dataSource: any = []
 
-  constructor() { }
+
+  displayedColumns = ['name_supplier','mon','tue','wed','thu','fri','sat','sun','qty','product_price','total_price','status','action'];
+
+  constructor(private WeeklyControlService: WeeklyControlService) { }
+
 
   ngOnInit(): void {
-    this.dataSource = new MatTableDataSource<any>(this.test);
+    this.WeeklyControlService.getWeeklyControlList().subscribe(data => {
+      this.dataSource = data;
+    })
   }
 
 }
