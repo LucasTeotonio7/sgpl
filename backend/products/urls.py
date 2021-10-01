8fa13b7a54b54ff5704cad8b74151e082dfc4d31
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-09-30 23:44:02 -0300

    criado endpoint parcial do controle semanal

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from products.views import views
 from products.views.views_weekly_collection \
-    import weekly_collection_api,purchase_api
+    import weekly_collection_api,purchase_api, weekly_collection
 
 urlpatterns=[
 
@@ -9,5 +9,6 @@ urlpatterns=[
     url(r'^product/([0-9]+)$',views.productApi),
     url(r'^product/choices/$',views.product_choices),
     url(r'^weekly-collection/(?P<date>[\w\-]+)/$', weekly_collection_api),
-    url(r'^purchase/(?P<date_start>[\w\-]+)/(?P<date_end>[\w\-]+)/$', purchase_api)
+    url(r'^purchase/(?P<date_start>[\w\-]+)/(?P<date_end>[\w\-]+)/$', purchase_api),
+    url(r'^weekly-collection-list/(?P<date_start>[\w\-]+)/(?P<date_end>[\w\-]+)/$', weekly_collection),
 ]
