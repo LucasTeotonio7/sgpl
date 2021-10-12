74c2fde718943ef3a2bfc63aa5880ab18d7ca454
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-10-12 15:17:13 -0300

    refatorado o componente weekly-control-form

--- frontend/src/app/components/weekly-control/weekly-control-form/weekly-control-form.component.ts
+++ frontend/src/app/components/weekly-control/weekly-control-form/weekly-control-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormactDate, string_to_datetime, stringToWeekday, dateWeekdayName } from '../../utils';
+import { WeeklyControlService } from '../services/weekly-control.service'
+import { Week, WeeklyCollection, weekView } from '../weekly-control-form/week.model';
+
 
 @Component({
   selector: 'sgpl-weekly-control-form',
@@ -7,9 +12,110 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeeklyControlFormComponent implements OnInit {
 
-  constructor() { }
+  WeeklyControlValues: any[] = []
+  dataSource: WeeklyCollection[] = []
 
-  ngOnInit(): void {
+  displayedColumns = [];
+
+  week: Week = {
+    date_start:'',
+    date_end:''
   }
 
+  weekView: weekView = {
+    date_start:null,
+    date_end:null
+  }
+
+  constructor(private WeeklyControlService: WeeklyControlService,
+              private route: ActivatedRoute,
+              private router: Router) { }
+
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if(id != null){
+      this.WeeklyControlService.getWeeklyCollection(id).subscribe(data => {
+        this.WeeklyControlValues = data;
+
+        this.WeeklyControlService.getWeek(id).subscribe(data=>{
+          this.week = data
+          this.FillDataSource(this.week, this.WeeklyControlValues)
+          this.generatedisplayedColumns(this.week.date_start)
+          this.weekView.date_start = string_to_datetime(this.week.date_start)
+          this.weekView.date_end = string_to_datetime(this.week.date_end)
+        })
+      });
+    }
+  }
+
+  cancelar(): void {
+    this.router.navigate(['/controle-semanal']);
+  }
+
+  salvar(): void {
+    for (var i=0; i < 7; i++) {
+      if(this.dataSource[i].id){
+        console.log(this.dataSource[i])
+        this.WeeklyControlService.updateWeeklyCollection(this.dataSource[i]).subscribe()
+      }else{
+        if(this.dataSource[i].quantity!== null){
+          this.WeeklyControlService.addWeeklyCollection(this.dataSource[i]).subscribe()
+        }
+      }
+    }
+    this.router.navigate(['/controle-semanal']);
+  }
+
+  generatedisplayedColumns(date_start){
+    var date = string_to_datetime(date_start)
+    for (var i = 0; i < 7; i++) {
+      this.displayedColumns.push(dateWeekdayName(date))
+      date.setDate(date.getDate() + 1)
+   }
+  }
+
+  FillDataSource(week, weeklyCollection){
+    var date_start = string_to_datetime(week.date_start)
+    var j=0
+
+    if(weeklyCollection.length>0){
+      for (var i=0; i < 7; i++) {
+        var date_array = string_to_datetime(weeklyCollection[j].date)
+        if(date_start.valueOf() === date_array.valueOf()){
+          this.dataSource.push(weeklyCollection[j])
+          if (weeklyCollection[j+1]){
+            j++
+          }
+        } else{
+          var new_collection: WeeklyCollection = {
+            date: FormactDate(date_start),
+            quantity:null,
+            purchase:1,
+          }
+          this.dataSource.push(new_collection)
+        }
+        date_start.setDate(date_start.getDate() + 1)
+      }
+    } else{
+      for (var i=0; i < 7; i++) {
+        var new_collection: WeeklyCollection = {
+          date: FormactDate(date_start),
+          quantity:null,
+          purchase:1,
+        }
+        this.dataSource.push(new_collection)
+      }
+    }
+
+  }
+
+  FormactDateMonthDay(data) {
+    data = new Date(data + ' 00:00:00');
+    var day = String(data.getDate()).padStart(2, '0');
+    var month = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"][data.getMonth()];
+    return day + ', ' + month;
+  }
 }
+
+
--- frontend/src/app/components/weekly-control/weekly-control-list/weekly-control-list.component.ts
+++ frontend/src/app/components/weekly-control/weekly-control-list/weekly-control-list.component.ts
@@ -10,13 +10,6 @@ import { WeeklyControlService } from '../services/weekly-control.service'
 export class WeeklyControlListComponent implements OnInit {
 
 
-
-  test: any = [
-    {name_supplier:'Lorem Ipsum',sat: '23',sun: '12',mon: '14',tue: '17',wed: '29',thu: '20',fri: '21',qty: '136',product_price: '1.95',total_price: '265.2',status: 'OK'},
-    {name_supplier:'Lorem Ipsum 2',sat: '233',sun: '132',mon: '134',tue: '137',wed: '293',thu: '320',fri: '231',qty: '136',product_price: '1.95',total_price: '2365.2',status: 'OK'},
-
-  ];
-
   dataSource: any = []
 
 
