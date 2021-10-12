605b721655972a9c5451eca9ce167e09b11e98ec
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-10-12 15:14:35 -0300

    criado models de weekly control

---
+++ frontend/src/app/components/weekly-control/weekly-control-form/week.model.ts
@@ -0,0 +1,17 @@
+export interface Week{
+    id?:number;
+    date_start:string;
+    date_end:string;
+}
+
+export interface weekView{
+    date_start:Date;
+    date_end:Date;
+}
+
+export interface WeeklyCollection{
+    id?:number;
+    date:string;
+    quantity:number;
+    purchase:number;
+}
