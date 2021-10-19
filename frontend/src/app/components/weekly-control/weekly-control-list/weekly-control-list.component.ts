62939452be921c63950f59573945a35a3aa5ecef
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-10-19 08:04:22 -0300

    adicionado nome do produto na view

--- frontend/src/app/components/weekly-control/weekly-control-list/weekly-control-list.component.ts
+++ frontend/src/app/components/weekly-control/weekly-control-list/weekly-control-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../product/models/product.model';
+import { ProductService } from 'src/app/components/product/services/product.service';
 import { string_to_datetime } from '../../utils';
 import { WeeklyControlService } from '../services/weekly-control.service'
 import { Week, weekView } from '../weekly-control-form/week.model';
@@ -24,9 +26,17 @@ export class WeeklyControlListComponent implements OnInit {
     product: null
   }
 
+  product: Product = {
+    name:'',
+    unit_measurement: '',
+    registration_date: '',
+    purchase_price: null
+  }
+
   displayedColumns = ['name_supplier','mon','tue','wed','thu','fri','sat','sun','qty','product_price','total_price','status','action'];
 
-  constructor(private WeeklyControlService: WeeklyControlService) { }
+  constructor(private WeeklyControlService: WeeklyControlService,
+              private ProductService: ProductService) { }
 
 
   ngOnInit(): void {
@@ -37,6 +47,12 @@ export class WeeklyControlListComponent implements OnInit {
       this.week = data;
       this.weekView.date_start = string_to_datetime(data.date_start)
       this.weekView.date_end = string_to_datetime(data.date_end)
+
+      this.ProductService.getProduct(data.product).subscribe(data => {
+        this.product = data;
+        console.log(this.product)
+      });
+
     })
 
   }
