4de1cb9c1925f8fe0f55673328d59546ded575f8
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-10-19 23:20:12 -0300

    criado modal de teste

--- frontend/src/app/components/weekly-control/weekly-control-list/weekly-control-list.component.ts
+++ frontend/src/app/components/weekly-control/weekly-control-list/weekly-control-list.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Product } from '../../product/models/product.model';
 import { ProductService } from 'src/app/components/product/services/product.service';
-import { string_to_datetime } from '../../utils';
+import { FormactDateMonthDay } from '../../utils';
 import { WeeklyControlService } from '../services/weekly-control.service'
-import { Week, weekView } from '../weekly-control-form/week.model';
+import { Week } from '../weekly-control-form/week.model';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  object: string;
+  name: string;
+}
 
 @Component({
   selector: 'sgpl-weekly-control-list',
@@ -15,9 +21,9 @@ export class WeeklyControlListComponent implements OnInit {
 
   dataSource: any = []
 
-  weekView: weekView = {
-    date_start:null,
-    date_end:null
+  weekView: any = {
+    date_start:'',
+    date_end:''
   }
 
   week: Week = {
@@ -33,10 +39,14 @@ export class WeeklyControlListComponent implements OnInit {
     purchase_price: null
   }
 
+  object: string;
+  name: string;
+
   displayedColumns = ['name_supplier','mon','tue','wed','thu','fri','sat','sun','qty','product_price','total_price','status','action'];
 
   constructor(private WeeklyControlService: WeeklyControlService,
-              private ProductService: ProductService) { }
+              private ProductService: ProductService,
+              public dialog: MatDialog) {}
 
 
   ngOnInit(): void {
@@ -45,8 +55,8 @@ export class WeeklyControlListComponent implements OnInit {
     })
     this.WeeklyControlService.getLastWeek().subscribe(data => {
       this.week = data;
-      this.weekView.date_start = string_to_datetime(data.date_start)
-      this.weekView.date_end = string_to_datetime(data.date_end)
+      this.weekView.date_start = FormactDateMonthDay(data.date_start)
+      this.weekView.date_end = FormactDateMonthDay(data.date_end)
 
       this.ProductService.getProduct(data.product).subscribe(data => {
         this.product = data;
@@ -56,5 +66,35 @@ export class WeeklyControlListComponent implements OnInit {
     })
 
   }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: {name: this.name, object: this.object}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.object = result;
+    });
+  }
+
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'modal.component.html',
+})
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+
 
 }
