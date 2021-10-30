d97d2a13e41030bf7dfd593bb4fc3bd2d241e089
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-10-29 21:09:26 -0300

    removido importação não utilizada

--- backend/weeklycontrol/urls.py
+++ backend/weeklycontrol/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from weeklycontrol.views.views_week import weektApi, get_week_purchase, \
-    get_last_week,next_week, get_weeks_to_product
+    get_last_week, get_weeks_to_product
 from weeklycontrol.views.views_weekly_collection \
     import weekly_collection_form, weekly_collection_list
 
@@ -12,7 +12,6 @@ urlpatterns=[
     url(r'^week-purchase/([0-9]+)$', get_week_purchase),
     url(r'^last-week/$',get_last_week),
     url(r'^weeks/([0-9]+)$', get_weeks_to_product),
-    url(r'^next-week/$',next_week),
     url(r'^weekly-collection-form/$',weekly_collection_form),
     url(r'^weekly-collection-list/(?P<date_start>[\w\-]+)/(?P<date_end>[\w\-]+)/(?P<id>[0-9]+)$', weekly_collection_list),
     url(r'^weekly-collection/(?P<date_start>[\w\-]+)/(?P<date_end>[\w\-]+)/$', weekly_collection)
